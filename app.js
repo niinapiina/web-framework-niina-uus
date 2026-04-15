58381216cbf9c0835b1c91a404fbe9e876c85328
niinapiina <niina.pietila@outlook.com>
2026-04-15 19:26:21 +0300

    Middlewaree ja sen semmost

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ const { engine } = require("express-handlebars");
 
 const app = express();
 const PORT = 3000;
+
+//Parsinnat ja staattiset filut
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Handlebar
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+// Datan luku JSONista
+const filePath = path.join(__dirname, "data", "movies.json");
+let movies = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+// Home route
+app.get("/", (req, res) => {
+res.redirect("/movies-page");
+});
