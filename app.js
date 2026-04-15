553a8317b004835f9a0a6e1658a8a4e3bfb6f803
niinapiina <niina.pietila@outlook.com>
2026-04-15 19:12:53 +0300

    ekaa puskua

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { engine } = require("express-handlebars");
+
+const app = express();
+const PORT = 3000;
